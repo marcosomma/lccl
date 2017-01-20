0db59c87663c3b596386185991b5978c4a4a867a
marcosomma <mak.so1979@gmail.com>
2017-01-20 10:34:21 +0100

    added circleci and code_climate conf#

--- test/index.test.js
+++ test/index.test.js
@@ -1,49 +1,49 @@
 const should = require('chai').should()
 
-const lcclSimple = require('../index')('test simple customization',false)
-const lcclSimpleDate = require('../index')('test simple customization with date',true)
+const lcclSimple = require('../index')('TEXT UNIQUE FOR ALL OF THEM',false)
+const lcclSimpleDate = require('../index')('TEXT UNIQUE FOR ALL OF THEM',true)
 
 const lcclConfig = {
-  info: 'TEST INFO',
-  warn: 'TEST WARNING',
-  error: 'TEST ERROR'
+  info: 'TEXT SPECIFIC INFO',
+  warn: 'TEXT SPECIFIC WARNING',
+  error: 'TEXT SPECIFIC ERROR'
 }
 const lcclObject = require('../index')(lcclConfig,false)
 const lcclObjectDate = require('../index')(lcclConfig,true)
 
 describe('# Test : Simple Customiztion', function() {
   it('passed', function (done) {
-    lcclSimple.log('test LOG')
-    lcclSimple.info('test INFO')
-    lcclSimple.warn('test WARN')
-    lcclSimple.error('test ERROR')
+    lcclSimple.log('TEST LOG')
+    lcclSimple.info('TEST INFO')
+    lcclSimple.warn('TEST WARN')
+    lcclSimple.error('TEST ERROR')
     done()
   });
 });
 describe('# Test : Simple Customiztion and Date', function() {
   it('passed', function (done) {
-    lcclSimpleDate.log('test LOG')
-    lcclSimpleDate.info('test INFO')
-    lcclSimpleDate.warn('test WARN')
-    lcclSimpleDate.error('test ERROR')
+    lcclSimpleDate.log('TEST LOG')
+    lcclSimpleDate.info('TEST INFO')
+    lcclSimpleDate.warn('TEST WARN')
+    lcclSimpleDate.error('TEST ERROR')
     done()
   });
 });
 describe('# Test : Specific Customiztion', function() {
   it('passed', function (done) {
-    lcclObject.log('test LOG')
-    lcclObject.info('test INFO')
-    lcclObject.warn('test WARN')
-    lcclObject.error('test ERROR')
+    lcclObject.log('TEST LOG')
+    lcclObject.info('TEST INFO')
+    lcclObject.warn('TEST WARN')
+    lcclObject.error('TEST ERROR')
     done()
   });
 });
 describe('# Test : Specific Customiztion and Date', function() {
   it('passed', function (done) {
-    lcclObjectDate.log('test LOG')
-    lcclObjectDate.info('test INFO')
-    lcclObjectDate.warn('test WARN')
-    lcclObjectDate.error('test ERROR')
+    lcclObjectDate.log('TEST LOG')
+    lcclObjectDate.info('TEST INFO')
+    lcclObjectDate.warn('TEST WARN')
+    lcclObjectDate.error('TEST ERROR')
     done()
   });
 });
